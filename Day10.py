2c2b1ba086c65ee51262e2c5890317e73b960a55
Vishnu Anand <vishnuanand2000@gmail.com>
2021-07-18 18:49:46 +0530

    Day 10 : Binary Numbers

---
+++ Day10.py
@@ -0,0 +1,24 @@
+# Day 10 : Binary Numbers
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    count = 0
+    
+    convert_to_binary = bin(n)
+    
+    while (n!=0):
+        n = (n & (n<<1))
+        count += 1
+        
+    
+    print(count)
