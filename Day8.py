085ace46ba290c2563b84fe484654f2859a2e278
Vishnu Anand <vishnuanand2000@gmail.com>
2021-07-18 18:48:31 +0530

    Day 8 : Dictionaries and Maps

---
+++ Day8.py
@@ -0,0 +1,21 @@
+# Day 8 : Dictionaries and Maps
+
+phonebook = {}
+entries = int(input())
+
+for n in range(entries):
+    name, num = input().strip().split(' ')
+    name, num = [str(name), int(num)]
+    phonebook[name] = num
+
+while True:
+    try:  
+        search = str(input())
+
+        if search in phonebook:
+            output = ''.join('%s=%r' % (search, phonebook[search]))
+            print(output) 
+        else:
+            print("Not found")
+    except EOFError:
+        break
