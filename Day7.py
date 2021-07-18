4766a3854212da16a646ff3c769bc66088692d76
Vishnu Anand <vishnuanand2000@gmail.com>
2021-07-18 18:47:11 +0530

    Day 7 : Arrays

---
+++ Day7.py
@@ -0,0 +1,19 @@
+# Day 7 : Arrays
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+    
+    for i in range(len(arr)-1, -1, -1):
+        print(str(arr[i]) + " ", end="")
