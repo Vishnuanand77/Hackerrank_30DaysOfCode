73892481f6b621f8577d379d79b7133862a7fe1a
Vishnu Anand <vishnuanand2000@gmail.com>
2021-07-21 23:32:41 +0530

    Day 21 : Generics

---
+++ 21_Day21.py
@@ -0,0 +1,5 @@
+# Day 21 : Generics
+
+# Solution cannot be solved in python. HackerRank has a restricted set of languages for this problem
+# I still have to learn what gennerics are and what they do!
+# Stay tuned!
