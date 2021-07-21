6481b1b228da8ec8a4aef981e5a9cc324c4f2485
Vishnu Anand <vishnuanand2000@gmail.com>
2021-07-21 23:01:16 +0530

    Day 9 : Recursion 3

--- 09_Day9.py
+++ 09_Day9.py
@@ -1,19 +1,10 @@
 # Day 9 : Recursion 3
 
-#!/bin/python3
-
 import math
 import os
 import random
 import re
 import sys
-
-#
-# Complete the 'factorial' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts INTEGER n as parameter.
-#
 
 def factorial(n):
     # Write your code here
